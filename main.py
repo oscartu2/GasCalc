a52e814cb3187c705d859c8ad4114b6aaa820a9d
Oscar Tu <o.car.w2@gmail.com>
2018-05-14 23:43:25 -0700

    Figuring out best API for gas prices

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 # Gas Calculator
 
 # Imports
-from urllib.request import Request, urlopen
-from bs4 import BeautifulSoup
+import requests
+import json
 
 def main():
 	#capacity = input("What is the gas capacity of your car? ")
-	site = 'https://www.gasbuddy.com/GasPrices/Washington/Seattle'
-	hdr = {'User-Agent': 'Mozilla/5.0'}
-	req = Request(site, headers=hdr)
-	page = urlopen(req)
-	soup = BeautifulSoup(page)
-	for td in soup.select('td'):
-		print(td)
+	api = 'http://devapi.mygasfeed.com/'
+	api +=  '/stations/radius/49/150/10/reg/price/rfej9napna.json'
+	my_response = requests.get(api)
+	print(my_response.text)
 
 
 if __name__ == "__main__":
