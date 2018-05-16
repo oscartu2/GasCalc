f218bdac0a972303b5f297e5c4a224c3d8c0d798
Oscar Tu <o.car.w2@gmail.com>
2018-05-15 23:50:18 -0700

    Add enum for mpg/L/100km

--- main.py
+++ main.py
@@ -4,6 +4,18 @@
 import requests
 import json
 
+class PriceIn(Enum):
+	CPG = 1
+	CPL = 0
+
+def measurement(choice):
+	if choice == '1':
+		return 1
+	elif choice == '0':
+		return 0
+	else:
+		return None
+
 def main():
 	price = input("What is the current price? (Enter in dollars, e.g. 3.88)")
 	capacity = input("What is the gas capacity of your car? ")
@@ -13,6 +25,7 @@ def main():
 		print("Invalid choice!")
 		choice = input("Enter 0 for Cents per Gallon, 1 for Cents per Litre: ")
 	# Enum 1 to mpg, 0 to litre
+	price = conversion(PriceIn.measurement(choice))
 	total_cost = price * capacity
 	print('Total cost will be: ' + str(total_cost) + ' to fill up.')
 
